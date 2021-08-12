6e79e946a42ced52dfeb598008db04a43e1fd10d
Brady Bott <bottbrady@gmail.com>
2021-08-12 12:24:44 -0600

    attemtping sending folder of images

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 5050
 
-app.get
+app.use(express.static('html'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../html/index.html'))
 
 app.listen(port, () => console.log(`We cruisin now boyyyysss, we out here on ${port}!!`))
