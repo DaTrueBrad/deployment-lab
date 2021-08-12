6ce8c88708fd0f125c6cd5eafac015edb06ccbe4
Brady Bott <bottbrady@gmail.com>
2021-08-12 12:16:42 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 5050
+
+app.get
+
+app.listen(port, () => console.log(`We cruisin now boyyyysss, we out here on ${port}!!`))
